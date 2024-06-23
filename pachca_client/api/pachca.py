1c42924f19dcc483f74e0849d7ac494720cd4c52
Andrey Shalashov <avshalashov@yandex.ru>
2024-06-23 23:38:06 +0300

    add list_all_chats, list_all_users

--- pachca_client/api/pachca.py
+++ pachca_client/api/pachca.py
@@ -1,4 +1,4 @@
-from typing import Any
+from typing import Any, Optional, Dict, List, Union
 
 from pachca_client.api.client import Client
 from pachca_client.api.cache import Cache
@@ -10,15 +10,47 @@ ENTITY_TYPE_USER = 'user'
 
 METHOD_CHATS = 'chats'
 METHOD_MESSAGES = 'messages'
-METHOD_PROFILE_STATUS = 'profile/status'
 METHOD_UPLOAD = 'uploads'
 METHOD_USERS = 'users'
 
 
 class Pachca:
-    def __init__(self, client: Client, cache: Cache = None):
+    def __init__(self, client: Client, cache: Cache = None) -> None:
         self.client = client
         self.cache = cache
+
+    def get_cached(self, scope: str) -> Any:
+        if self.cache is None:
+            return None
+        return self.cache.get(scope)
+
+    def get_message(self, message_id) -> Optional[Dict]:
+        method = f'{METHOD_MESSAGES}/{message_id}'
+        return self.client.call_api_get(method)
+
+    def get_chat(self, chat_id: Union[str, int]) -> Optional[Dict]:
+        if isinstance(chat_id, str):
+            chat_id = self.resolve_chat_name(chat_id)
+        method = f'{METHOD_CHATS}/{chat_id}'
+        return self.client.call_api_get(method)
+
+    def resolve_chat_name(self, name: str) -> Optional[int]:
+        chats = self.get_cached(METHOD_CHATS)
+        if chats is None:
+            chats = self.list_all_chats()
+        for chat in chats:
+            if chat['name'] == name:
+                return chat['id']
+        return None
+
+    def resolve_user_name(self, name: str) -> Optional[int]:
+        users = self.get_cached(METHOD_USERS)
+        if users is None:
+            users = self.list_all_users()
+        for user in users:
+            if user['nickname'] == name:
+                return user['id']
+        return None
 
     def set_cached(self, scope: str, value: Any) -> Any:
         if self.cache is None:
@@ -26,55 +58,83 @@ class Pachca:
         self.cache.update(scope, value)
         return value
 
-    def get_cached(self, scope: str) -> Any:
-        if self.cache is None:
-            return None
-        return self.cache.get(scope)
+    def list_all_chats(self, per: int = 50) -> List:
+        page = 1
+        chats = []
+        while True:
+            response = self.list_chats(per, page)
+            chats.extend(response)
+            if len(response) == per:
+                page += 1
+                continue
+            break
+        return self.set_cached(METHOD_CHATS, chats)
 
-    def resolve_chat_name(self, name):
-        chats = self.get_cached(METHOD_CHATS)
-        if chats is None:
-            chats = self.list_chats()
-        for chat in chats:
-            if chat['name'] == name:
-                return chat['id']
-        return None
+    def list_all_users(self, per: int = 50) -> List:
+        page = 1
+        users = []
+        while True:
+            response = self.list_users(per=per, page=page)
+            users.extend(response)
+            if len(response) == per:
+                page += 1
+                continue
+            break
+        return self.set_cached(METHOD_USERS, users)
 
-    def resolve_user_name(self, name):
-        users = self.get_cached(METHOD_USERS)
-        if users is None:
-            users = self.list_users()
-        for user in users:
-            if user['nickname'] == name:
-                return user['id']
-        return None
+    def list_chats(self,
+                   per: int = 50,
+                   page: int = 1,
+                   availability: str = 'is_member',
+                   last_message_at_after: Optional[str] = None,
+                   last_message_at_before: Optional[str] = None) -> List:
+        payload = {
+            'per': per,
+            'page': page,
+            'availability': availability}
+        if last_message_at_after:
+            payload['last_message_at_after'] = last_message_at_after
+        if last_message_at_before:
+            payload['last_message_at_before'] = last_message_at_before
+        response = self.client.call_api_get(METHOD_CHATS, payload)
+        if response is None:
+            return []
+        return response
 
-    def list_chats(self):
-        return self.set_cached(METHOD_CHATS, self.client.call_api_get(METHOD_CHATS))
+    def list_messages(self, chat_id: int, per: int = 50, page: int = 1) -> Optional[List]:
+        payload = {
+            'chat_id': chat_id,
+            'per': per,
+            'page': page}
+        return self.client.call_api_get(METHOD_MESSAGES, payload)
 
-    def list_users(self):
-        return self.set_cached(METHOD_USERS, self.client.call_api_get(METHOD_USERS))
-
-    def get_status(self):
-        return self.client.call_api_get(METHOD_PROFILE_STATUS)
+    def list_users(self, per: int = 50, page: int = 1, query: Optional[str] = None) -> List:
+        payload = {
+            'per': per,
+            'page': page
+        }
+        if query:
+            payload['query'] = query
+        response = self.client.call_api_get(METHOD_USERS, payload)
+        if response is None:
+            return []
+        return response
 
     def new_message(self,
+                    entity_id: Union[str, int],
                     content: str,
                     entity_type: str = ENTITY_TYPE_DISCUSSION,
-                    entity_name: str = '',
-                    entity_id: int = None,
                     parent_message_id: int = None,
-                    files: list[File] = []):
-        if entity_id is None and len(entity_name) > 0:
+                    files: List[File] = []) -> Optional[Dict]:
+        if isinstance(entity_id, str):
             if entity_type == ENTITY_TYPE_DISCUSSION:
-                entity_id = self.resolve_chat_name(entity_name)
+                entity_id = self.resolve_chat_name(entity_id)
             elif entity_type == ENTITY_TYPE_USER:
-                entity_id = self.resolve_user_name(entity_name)
+                entity_id = self.resolve_user_name(entity_id)
         message = {
             'entity_type': entity_type,
             'content': content,
-            'entity_id': entity_id
-            }
+            'entity_id': entity_id}
         if parent_message_id is not None:
             message['parent_message_id'] = parent_message_id
         if len(files) != 0:
@@ -84,24 +144,19 @@ class Pachca:
                 file.prepare(file_info['key'])
                 message['files'].append(file.as_dict())
         payload = {
-            'message': message
-        }
+            'message': message}
         return self.client.call_api_post(METHOD_MESSAGES, payload)
 
-    def new_thread(self, id: int):
-        method = f'{METHOD_USERS}/{id}/thread'
+    def new_thread(self, id: int) -> Optional[Dict]:
+        method = f'{METHOD_MESSAGES}/{id}/thread'
         return self.client.call_api_post(method)
 
-    def get_message(self, message_id):
-        method = f'{METHOD_MESSAGES}/{message_id}'
-        return self.client.call_api_get(method)
-
-    def upload(self, file: File):
+    def upload(self, file: File) -> Optional[Dict]:
         # get pre-sign object
         info = self.client.call_api_post(METHOD_UPLOAD)
         # upload file
         url = info['direct_url']
         del info['direct_url']
         with open(file.path, 'rb') as f:
-            response = self.client.upload(url, f, info)            
+            self.client.upload(url, f, info)            
         return info
---
+++ tests/tests_pachca.py
@@ -0,0 +1,76 @@
+import unittest.mock as mock
+import unittest
+from pachca_client import get_pachca
+
+
+class TestResolveName(unittest.TestCase):
+    def setUp(self):
+        self.pachca = get_pachca('')
+
+    def test_resolve_user_name(self):
+        self.pachca.get_cached = mock.MagicMock()
+        self.pachca.get_cached.return_value = None
+        self.pachca.list_all_users = mock.MagicMock()
+        self.pachca.list_all_users.return_value = [
+            {'nickname': 'andrey', 'id': 100}, {'nickname': 'sergey', 'id': 200}
+        ]
+        self.assertEqual(self.pachca.resolve_user_name('andrey'), 100)
+        self.assertEqual(self.pachca.resolve_user_name('dmitriy'), None)
+
+    def test_resolve_user_name_cached(self):
+        self.pachca.get_cached = mock.MagicMock()
+        self.pachca.get_cached.return_value = [
+            {'nickname': 'andrey', 'id': 100}, {'nickname': 'sergey', 'id': 200}
+        ]
+        self.assertEqual(self.pachca.resolve_user_name('andrey'), 100)
+        self.assertEqual(self.pachca.resolve_user_name('dmitriy'), None)
+
+    def test_resolve_chat_name(self):
+        self.pachca.get_cached = mock.MagicMock()
+        self.pachca.get_cached.return_value = None
+        self.pachca.list_all_chats = mock.MagicMock()
+        self.pachca.list_all_chats.return_value = [
+            {'name': 'Chat1', 'id': 100}, {'name': 'Chat2', 'id': 200}
+        ]
+        self.assertEqual(self.pachca.resolve_chat_name('Chat1'), 100)
+        self.assertEqual(self.pachca.resolve_chat_name('Chat3'), None)
+
+    def test_resolve_chat_name_cached(self):
+        self.pachca.get_cached = mock.MagicMock()
+        self.pachca.get_cached.return_value = [
+            {'name': 'Chat1', 'id': 100}, {'name': 'Chat2', 'id': 200}
+        ]
+        self.assertEqual(self.pachca.resolve_chat_name('Chat1'), 100)
+        self.assertEqual(self.pachca.resolve_chat_name('Chat3'), None)
+
+
+class TestChats(unittest.TestCase):
+    def setUp(self):
+        self.pachca = get_pachca('')
+
+    def test_get_chat_by_id(self):
+        self.pachca.client.call_api_get = mock.MagicMock()
+        self.pachca.client.call_api_get.return_value = {'name': 'Chat1', 'id': 100}
+        self.assertDictEqual(self.pachca.get_chat(100), {'name': 'Chat1', 'id': 100})
+
+    def test_get_chat_by_name(self):
+        self.pachca.client.call_api_get = mock.MagicMock()
+        self.pachca.client.call_api_get.return_value = {'name': 'Chat1', 'id': 100}
+        self.pachca.resolve_chat_name = mock.MagicMock()
+        self.pachca.resolve_chat_name.return_value = 100
+        self.assertDictEqual(self.pachca.get_chat('Chat1'), {'name': 'Chat1', 'id': 100})
+        self.pachca.resolve_chat_name.assert_called_once()
+        self.pachca.client.call_api_get.assert_called_once_with('chats/100')
+
+    def test_list_all_chats(self):
+        self.pachca.list_chats = mock.MagicMock()
+
+        def mock_list_chats(per: int, page: int):
+            if page == 1:
+                return [{'name': 'Chat1', 'id': 100}, {'name': 'Chat2', 'id': 200}]
+            if page == 2:
+                return [{'name': 'Chat3', 'id': 300}]
+
+        self.pachca.list_chats.side_effect = mock_list_chats
+        chats = self.pachca.list_all_chats(per=2)
+        self.assertListEqual(chats, [{'name': 'Chat1', 'id': 100}, {'name': 'Chat2', 'id': 200}, {'name': 'Chat3', 'id': 300}])
